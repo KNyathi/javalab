7804df386a2a0e2a24ed3151c3c3c3aa6654fd0d
KNyathi <khayelihlenyathi07@gmail.com>
2023-10-22 10:19:48 +0300

    finally solved the error

--- lab5/exercise2.java
+++ lab5/exercise2.java
@@ -1,32 +1,36 @@
 import java.util.regex.*;
 
 public class exercise2 {
-    public static void main(String[] args) {
-        String password = "YourPassword"; // Replace with the entered password
+    public static boolean isPasswordValid(String password) {
+        boolean lengthCondition = password.length() >= 8 && password.length() <= 16;
+        boolean uppercaseCondition = password.matches(".*[A-Z].*");
+        boolean digitCondition = password.matches(".*\\d.*");
 
         try {
-            validatePassword(password);
-            System.out.println("Password is valid.");
-        } catch (IllegalArgumentException e) {
-            System.out.println("Password is invalid: " + e.getMessage());
+            if (!lengthCondition) {
+                throw new Exception("Password must be between 8 and 16 characters long.");
+            }
+            if (!uppercaseCondition) {
+                throw new Exception("Password must contain at least one uppercase letter.");
+            }
+            if (!digitCondition) {
+                throw new Exception("Password must contain at least one digit.");
+            }
+
+            return true; // Password is valid
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+            return false; // Password is invalid
         }
     }
 
-    public static void validatePassword(String password) {
-        try {
-            if (!password.matches(".[A-Z].")) {
-                throw new IllegalArgumentException("Password must contain at least one uppercase letter.");
-            }
+    public static void main(String[] args) {
+        String password = "YourPassword123HHHHHH";
 
-            if (!password.matches(".\\d.")) {
-                throw new IllegalArgumentException("Password must contain at least one digit.");
-            }
-
-            if (password.length() < 8 || password.length() > 16) {
-                throw new IllegalArgumentException("Password length must be between 8 and 16 characters.");
-            }
-        } catch (IllegalArgumentException e) {
-            throw e;
+        if (isPasswordValid(password)) {
+            System.out.println("Password is valid.");
+        } else {
+            System.out.println("Password is invalid.");
         }
     }
 }
